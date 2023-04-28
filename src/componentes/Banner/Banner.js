d7337caa8694a863708c42f75ce1d53dc2eee8fd
vidadofael <vidadofael@gmail.com>
2023-04-28 15:51:48 -0300

    organizando arquivos

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/ALURABEY.png" alt="banner principal da página do organo2"/>
+        </header>
+    )
+}
+
+export default Banner
